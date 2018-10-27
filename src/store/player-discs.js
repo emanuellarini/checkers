8e92c2f37f6993c031b1085d425684d32cb19538
emanuellarini <emanuellarini@hotmail.com>
2018-10-27 20:16:34 -0300

    [refactor] using player from action

--- src/store/player-discs.js
+++ src/store/player-discs.js
@@ -27,38 +27,38 @@ export const updatePlayerDiscs = ({player, ...args}) => {
   }
 }
 
-export default function(player) {
-  const initialState = {
-    1: {
-      grey1: [5, 0],
-      grey2: [5, 2],
-      grey3: [5, 4],
-      grey4: [5, 6],
-      grey5: [6, 1],
-      grey6: [6, 3],
-      grey7: [6, 5],
-      grey8: [6, 7],
-      grey9: [7, 0],
-      grey10: [7, 2],
-      grey11: [7, 4],
-      grey12: [7, 6],
-    },
-    2: {
-      red1: [0, 1],
-      red2: [0, 3],
-      red3: [0, 5],
-      red4: [0, 7],
-      red5: [1, 0],
-      red6: [1, 2],
-      red7: [1, 4],
-      red8: [1, 6],
-      red9: [2, 1],
-      red10: [2, 3],
-      red11: [2, 5],
-      red12: [2, 7],
-    },
-  }
+const initialState = {
+  1: {
+    grey1: [5, 0],
+    grey2: [5, 2],
+    grey3: [5, 4],
+    grey4: [5, 6],
+    grey5: [6, 1],
+    grey6: [6, 3],
+    grey7: [6, 5],
+    grey8: [6, 7],
+    grey9: [7, 0],
+    grey10: [7, 2],
+    grey11: [7, 4],
+    grey12: [7, 6],
+  },
+  2: {
+    red1: [0, 1],
+    red2: [0, 3],
+    red3: [0, 5],
+    red4: [0, 7],
+    red5: [1, 0],
+    red6: [1, 2],
+    red7: [1, 4],
+    red8: [1, 6],
+    red9: [2, 1],
+    red10: [2, 3],
+    red11: [2, 5],
+    red12: [2, 7],
+  },
+}
 
+export default function(player) {
   return function reducer(state = initialState[player], action) {
     const isInitializationCall = state === undefined
     if (action.player !== player && !isInitializationCall) {
@@ -66,8 +66,7 @@ export default function(player) {
     }
 
     switch (action.type) {
-      case playersTypes[1].UPDATE:
-      case playersTypes[2].UPDATE: {
+      case playersTypes[action.player].UPDATE: {
         const {disc, coords} = action.payload
 
         return {
@@ -75,8 +74,7 @@ export default function(player) {
           [disc]: coords,
         }
       }
-      case playersTypes[1].REMOVE:
-      case playersTypes[2].REMOVE: {
+      case playersTypes[action.player].REMOVE: {
         return _omit(state, action.payload.discs)
       }
       default:
