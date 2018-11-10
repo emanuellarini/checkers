6a0d13a3608d0357c199e47f9fa6d9515b4a4644
emanuellarini <emanuellarini@hotmail.com>
2018-11-10 14:41:41 -0200

    [remove] unused css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import Game from './components/Game'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import {Provider} from 'react-redux'
