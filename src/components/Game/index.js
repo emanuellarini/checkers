979a3cbf4e87f80a1ad9ef59a41ea03fd6e1a5c3
emanuellarini <emanuellarini@hotmail.com>
2018-11-12 18:49:56 -0200

    [add] fade in board

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -4,27 +4,31 @@ import AppBar from 'components/AppBar'
 import Board from 'components/Board'
 import Winner from 'components/WinnerAlert'
 import Turn from 'components/Turn'
+import Zoom from '@material-ui/core/Zoom'
 import Notification from 'components/Notification'
 import StyledGame from './styled'
 
 function Game() {
+  const fadeDelay = {transitionDelay: 700}
   return (
     <StyledGame>
       <AppBar />
 
-      <div className="Container">
-        <Board />
-        <Typography
-          variant="caption"
-          color="textSecondary"
-          align="center"
-          component="p"
-          className="Hint"
-        >
-          hint: you can pass the turn by <b>pressing Spacebar</b> after your
-          move
-        </Typography>
-      </div>
+      <Zoom in appear style={fadeDelay}>
+        <div className="Container">
+          <Board />
+          <Typography
+            variant="caption"
+            color="textSecondary"
+            align="center"
+            component="p"
+            className="Hint"
+          >
+            hint: you can pass the turn by <b>pressing Spacebar</b> after your
+            move
+          </Typography>
+        </div>
+      </Zoom>
 
       <Winner />
       <Turn />
