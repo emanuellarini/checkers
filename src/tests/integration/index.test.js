6b90bdd1a5a6799c96e1682eab5e8d65443c802f
emanuellarini <emanuellarini@hotmail.com>
2018-11-19 18:26:41 -0200

    [tests] add first integration test

---
+++ src/tests/integration/index.test.js
@@ -0,0 +1,37 @@
+import {initializeBrowser} from 'utils/tests'
+
+describe('The Game Movements', () => {
+  test('player 1 can move its disc to the upper right square towards opponent', async () => {
+    const {page, browser} = await initializeBrowser()
+
+    const dropSquare = await page.waitForSelector(
+      '[data-testid="board-square-4-3"]',
+    )
+    const player1Disc = await page.waitForSelector(
+      '[data-testid="board-square-5-2"]',
+    )
+    const dropSquareBox = await dropSquare.boundingBox()
+    const player1DiscBox = await player1Disc.boundingBox()
+
+    await page.mouse.move(
+      player1DiscBox.x + player1DiscBox.width / 2,
+      player1DiscBox.y + player1DiscBox.height / 2,
+    )
+    await page.mouse.down()
+    await page.mouse.move(
+      dropSquareBox.x + dropSquareBox.width / 2,
+      dropSquareBox.y + dropSquareBox.height / 2,
+      {steps: 6},
+    )
+    await page.mouse.up()
+    await page.waitFor(100)
+
+    const dropSquareHTML = await page.$eval(
+      '[data-testid="board-square-4-3"]',
+      e => e.innerHTML,
+    )
+    expect(dropSquareHTML).toContain('disc-player-1-grey2')
+
+    await browser.close()
+  })
+})
--- src/utils/tests.js
+++ src/utils/tests.js
@@ -2,9 +2,60 @@ import React from 'react'
 import Provider from 'components/Provider'
 import {createStore} from 'redux'
 import reducers from 'store'
+import i18n from 'i18next'
+import {I18nextProvider} from 'react-i18next'
+import puppeteer from 'puppeteer'
+jest.setTimeout(10000)
 
+const translator = i18n.init({
+  fallbackLng: 'en',
+  debug: false,
+  saveMissing: false,
+
+  interpolation: {
+    escapeValue: false,
+  },
+
+  react: {
+    wait: false,
+    nsMode: 'fallback',
+  },
+})
+
+/**
+ * Add Providers as wrappers for testing
+ *
+ * @param props
+ * @returns {*}
+ */
 export const Wrapper = props => {
   const store = createStore(reducers, props.store || {})
 
-  return <Provider store={store}>{props.children}</Provider>
+  return (
+    <Provider store={store}>
+      <I18nextProvider i18n={translator}>{props.children}</I18nextProvider>
+    </Provider>
+  )
+}
+
+/**
+ * Initialize browser and retrieve its configurations
+ *
+ * @returns {Promise<{browser, page}>}
+ */
+export const initializeBrowser = async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    slowMo: 100,
+    args: [`--window-size=${1440},${900}`],
+  })
+  const page = await browser.newPage()
+  await page.setViewport({width: 1440, height: 900, deviceScaleFactor: 1})
+  await page.goto('http://localhost:3000')
+  await page.waitFor(900)
+
+  return {
+    browser,
+    page,
+  }
 }
