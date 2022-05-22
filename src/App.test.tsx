a623d89f87df1c6061f22cbb5347af7a2e8407bb
Emanuel Larini <emanuellarini@hotmail.com>
2022-05-21 23:28:42 -0300

    refactor: export App as const

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
+import { App } from './App';
 
 describe('App', () => {
   describe('Elements', () => {
--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,25 @@ import { Header, Board, Turn } from './components';
 import { GameProvider } from './context';
 import { theme } from './lib/theme';
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <GameProvider>
-        <Header />
-        <Container sx={{ mt: 4, mb: 4 }}>
-          <Paper
-            elevation={4}
-            sx={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(8, 1fr)',
-              justifyContent: 'center',
-              border: '1.25em ridge #492510'
-            }}
-          >
-            <Board />
-            <Turn />
-          </Paper>
-        </Container>
-      </GameProvider>
-    </ThemeProvider>
-  );
-}
-
-export default App;
+export const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <GameProvider>
+      <Header />
+      <Container sx={{ mt: 4, mb: 4 }}>
+        <Paper
+          elevation={4}
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(8, 1fr)',
+            justifyContent: 'center',
+            border: '1.25em ridge #492510'
+          }}
+        >
+          <Board />
+          <Turn />
+        </Paper>
+      </Container>
+    </GameProvider>
+  </ThemeProvider>
+);
