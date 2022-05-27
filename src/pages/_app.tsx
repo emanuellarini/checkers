f4c8924bb049ff41ec169497693413dff63c49cf
Emanuel Larini <emanuellarini@hotmail.com>
2022-05-26 23:03:56 -0300

    fix: favicon not displaying

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,7 @@ const App = (props: MyAppProps) => {
     <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
