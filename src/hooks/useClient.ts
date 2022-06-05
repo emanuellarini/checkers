fd5a27e719b410e7a8bbe7448801aa14e3e7f5c6
Emanuel Larini <emanuellarini@hotmail.com>
2022-06-05 17:37:45 -0300

    fix: remove unneeded config in Colyseus Client

--- src/hooks/useClient.ts
+++ src/hooks/useClient.ts
@@ -3,8 +3,5 @@ import { useMemo } from 'react';
 import { Client } from 'colyseus.js';
 
 export const useClient = () => {
-  return useMemo(
-    () => new Client(`ws://localhost:${process.env.PORT || 3000}`),
-    []
-  );
+  return useMemo(() => new Client(), []);
 };
